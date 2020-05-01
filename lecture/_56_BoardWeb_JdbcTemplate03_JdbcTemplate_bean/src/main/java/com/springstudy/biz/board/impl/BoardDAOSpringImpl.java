728ec230e9b8de4f4c30c83a126e373f60c737a8
junyoung4213 <junyoung4213@gmail.com>
2020-05-01 13:10:01 +0900

    JdbcTemplate_bean

---
+++ lecture/_56_BoardWeb_JdbcTemplate03_JdbcTemplate_bean/src/main/java/com/springstudy/biz/board/impl/BoardDAOSpringImpl.java
@@ -0,0 +1,60 @@
+package com.springstudy.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.springstudy.biz.board.BoardDAO;
+import com.springstudy.biz.board.BoardVO;
+
+@Repository("boardDAO")
+public class BoardDAOSpringImpl implements BoardDAO {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	// SQL 명령어들
+	private final String BOARD_INSERT = "INSERT INTO BOARD(seq,title,writer,content) VALUES((SELECT NVL(MAX(seq),0)+1 FROM board), ?,?,?)";
+	private final String BOARD_UPDATE = "UPDATE board SET title = ?, content=?, WHERE seq=?";
+	private final String BOARD_DELETE = "DELETE board WHERE seq=?";
+	private final String BOARD_GET = "SELECT * FROM board WHERE seq=?";
+	private final String BOARD_LIST = "SELECT * FROM board ORDER BY seq DESC";
+
+	@Override
+	public void insertBoard(BoardVO vo) {
+		System.out.println("===> Spring JDBC1로 insertBoard() 기능 처리");
+		jdbcTemplate.update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
+
+	}
+
+	@Override
+	public void updateBoard(BoardVO vo) {
+		System.out.println("===> Spring JDBC1로 updateBoard() 기능 처리");
+		jdbcTemplate.update(BOARD_UPDATE, vo.getTitle(), vo.getContent(), vo.getSeq());
+
+	}
+
+	@Override
+	public void deleteBoard(BoardVO vo) {
+		System.out.println("===> Spring JDBC1로 deleteBoard() 기능 처리");
+		jdbcTemplate.update(BOARD_DELETE, vo.getSeq());
+
+	}
+
+	@Override
+	public BoardVO getBoard(BoardVO vo) {
+		System.out.println("===> Spring JDBC1로 getBoard() 기능 처리");
+		Object[] args = { vo.getSeq() };
+
+		return jdbcTemplate.queryForObject(BOARD_GET, args, new BoardRowMapper());
+	}
+
+	@Override
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		System.out.println("===> Spring JDBC1로 getBoardList() 기능 처리");
+		return jdbcTemplate.query(BOARD_LIST, new BoardRowMapper());
+	}
+
+}
---
+++ lecture/_56_BoardWeb_JdbcTemplate03_JdbcTemplate_bean/src/main/java/com/springstudy/biz/board/impl/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package com.springstudy.biz.board.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springstudy.biz.board.BoardDAO;
+import com.springstudy.biz.board.BoardService;
+import com.springstudy.biz.board.BoardVO;
+import com.springstudy.biz.common.log.LogAdvice;
+
+/*
+ * 기존에 비즈니스 로직 전후에 로그를 출력하기 위해서 
+ * 로그 객체를 생성해서 biz로직 전후에 로그 메서드를
+ * 호출하는 식으로 프로그래밍 했다.
+*/
+
+@Service("boardService")
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardDAO boardDAO;
+
+	@Override
+	public void insertBoard(BoardVO vo) {
+		boardDAO.insertBoard(vo);
+	}
+
+	@Override
+	public void updateBoard(BoardVO vo) {
+		boardDAO.updateBoard(vo);
+
+	}
+
+	@Override
+	public void deleteBoard(BoardVO vo) {
+		boardDAO.deleteBoard(vo);
+	}
+
+	@Override
+	public BoardVO getBoard(BoardVO vo) {
+		return boardDAO.getBoard(vo);
+	}
+
+	@Override
+	public List<BoardVO> getBoardList(BoardVO vo) {
+//		if (vo != null)
+//			throw new IllegalArgumentException();
+		return boardDAO.getBoardList(vo);
+	}
+
+}
