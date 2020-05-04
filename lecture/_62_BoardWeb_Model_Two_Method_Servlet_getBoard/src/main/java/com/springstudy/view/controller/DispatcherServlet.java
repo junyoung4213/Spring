ab0300074e68492c67c8f3b46b12e6a687fe805f
junyoung4213 <junyoung4213@gmail.com>
2020-05-04 11:06:14 +0900

    login_getBoardList

---
+++ lecture/_62_BoardWeb_Model_Two_Method_Servlet_getBoard/src/main/java/com/springstudy/view/controller/DispatcherServlet.java
@@ -0,0 +1,119 @@
+package com.springstudy.view.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.springstudy.biz.board.BoardDAO;
+import com.springstudy.biz.board.BoardVO;
+import com.springstudy.biz.board.impl.BoardDAOImpl;
+import com.springstudy.biz.user.UserDAO;
+import com.springstudy.biz.user.UserVO;
+import com.springstudy.biz.user.impl.UserDAOImpl;
+
+//@WebServlet(name = "action", urlPatterns = { "*.do" })
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		process(request, response);
+	}
+
+	private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// 1. 클라이언트 요청 path 정보를 추출한다
+		String uri = request.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		System.out.println(path);
+
+		// 2. 클라이언트 요청 path에 따라 분기한다
+		if (path.equals("/login.do")) {
+			login(request, response);
+		} else if (path.equals("/logout.do")) {
+			logout(request, response);
+		} else if (path.equals("/insertBoard.do")) {
+			insertBoard(request, response);
+		} else if (path.equals("/updateBoard.do")) {
+			updateBoard(request, response);
+		} else if (path.equals("/deleteBoard.do")) {
+			deleteBoard(request, response);
+		} else if (path.equals("/getBoard.do")) {
+			getBoard(request, response);
+		} else if (path.equals("/getBoardList.do")) {
+			getBoardList(request, response);
+		}
+	}
+
+	// login.jsp가 처리해달라고 요청하는 업무 담당
+	private void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("로그인 처리");
+
+		// 1. 사용자 입력 정보 추출
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+
+		// 2. DB에 접속해서 사용자가 있는지 확인
+		UserVO vo = new UserVO();
+		vo.setId(id);
+		vo.setPassword(password);
+
+		UserDAO userDAO = new UserDAOImpl();
+		UserVO user = userDAO.getUser(vo);
+
+		// 사용자가 DB에 있을 때
+		if (user != null) {
+			response.sendRedirect("getBoardList.do");
+		}
+		// 사용자가 DB에 없을 때
+		else {
+			response.sendRedirect("login.jsp");
+		}
+
+	}
+
+	private void logout(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("로그아웃 처리");
+	}
+
+	private void insertBoard(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("글 등록 처리");
+	}
+
+	private void updateBoard(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("글 수정 처리");
+	}
+
+	private void deleteBoard(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("글 삭제 처리");
+	}
+
+	private void getBoard(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("글 상세 조회 처리");
+	}
+
+	private void getBoardList(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		System.out.println("글 목록 검색 처리");
+
+		// 1. 글 목록 정보들을 DB로부터 꺼낸다.
+		BoardVO vo = new BoardVO();
+		BoardDAO boardDAO = new BoardDAOImpl();
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+
+		// 2. JSP가 읽을 수 있도록 공유한다.
+		HttpSession session = request.getSession();
+		session.setAttribute("boardList", boardList);
+		response.sendRedirect("getBoardList.jsp");
+	}
+
+}
---
+++ lecture/_62_BoardWeb_Model_Two_Method_Servlet_getBoard/src/main/webapp/login_proc.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="com.springstudy.biz.user.UserDAO"%>
+<%@ page import="com.springstudy.biz.user.impl.UserDAOImpl"%>
+<%@ page import="com.springstudy.biz.user.UserVO"%>
+
+<%
+	String id = request.getParameter("id");
+	String password = request.getParameter("password");
+	
+	UserVO vo = new UserVO();
+	vo.setId(id);
+	vo.setPassword(password);
+	
+	UserDAO userDAO = new UserDAOImpl();
+	UserVO user = userDAO.getUser(vo);
+	
+	if(user != null){
+		response.sendRedirect("getBoardList.jsp");
+	}else{
+		response.sendRedirect("login.jsp");
+	}
+%>
